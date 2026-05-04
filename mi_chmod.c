fe053876c0ec1828833ef8894428e7f398a84953
sud0cs <asintessintes@gmail.com>
2026-05-04 17:12:53 +0200

    swap args

--- mi_chmod.c
+++ mi_chmod.c
@@ -1,14 +1,14 @@
 #include "directorios.h"
 int main(int argc, char **argv){
     if(argc<4){
-	printf("mi_chmod <disco> <path> <permisos>\n");
+	printf("mi_chmod <disco> <permisos> <path>\n");
 	return EXITO;
     }
     bmount(argv[1]);
-    unsigned char perms = atoi(argv[3]);
+    unsigned char perms = atoi(argv[2]);
     if(perms>7){
 	xpperror("Valor máximo para los permisos: 7", RED, DEFAULT, true, false);
     }
-    mi_chmod(argv[2], perms);
+    mi_chmod(argv[3], perms);
     bumount();
 }
--- mi_mkdir.c
+++ mi_mkdir.c
@@ -1,12 +1,12 @@
 #include "directorios.h"
 int main(int argc, char **argv){
     if(argc<4){
-	printf("mi_mkdir <disco> <path> <permisos>\n*nota: mkdir imita el comportamiento del programa de linux por lo tanto aunque el path no acabe en / creará un directorio.\nPara crear archivos utilizar mi_touch");
+	printf("mi_mkdir <disco> <permisos> <path>\n*nota: mkdir imita el comportamiento del programa de linux por lo tanto aunque el path no acabe en / creará un directorio.\nPara crear archivos utilizar mi_touch");
 	return EXITO;
     }
     bmount(argv[1]);
-    char *path = argv[2];
-    char perms = atoi(argv[3]);
+    char *path = argv[3];
+    char perms = atoi(argv[2]);
     if(path[strlen(path)-1] != '/')strcat(path, "/");
     if(perms>7 || perms<0)return FALLO;
     print_dir_error(mi_creat(path, perms));
